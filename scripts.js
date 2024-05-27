7fbb3388ac346e9155e47b7f4a2a01caabe40125
msemaska <martynasmasliukas@gmail.com>
2024-05-27 12:39:51 +0300

    fixed transparent bug for iphone

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.querySelector('.nav-toggle');
     const navLinks = document.querySelector('.navLinks');
@@ -8,39 +5,38 @@ document.addEventListener('DOMContentLoaded', function() {
     navToggle.addEventListener('click', function() {
         navLinks.classList.toggle('active'); // This should toggle the visibility
     });
-});
 
-document.getElementById('contactForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+    const contactForm = document.getElementById('contactForm');
+    if (contactForm) {
+        contactForm.addEventListener('submit', async function(event) {
+            event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const formProps = Object.fromEntries(formData);
-    formProps.access_key = '5fd3f272-ff6f-410a-b25b-548a6911e3df';
+            const formData = new FormData(event.target);
+            const formProps = Object.fromEntries(formData);
+            formProps.access_key = '5fd3f272-ff6f-410a-b25b-548a6911e3df';
 
-    try {
-        const response = await fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(formProps)
+            try {
+                const response = await fetch('https://api.web3forms.com/submit', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                    },
+                    body: JSON.stringify(formProps)
+                });
+
+                const result = await response.json();
+                if (result.success) {
+                    document.getElementById('contactForm').style.display = 'none';
+                    document.getElementById('thankYouMessage').style.display = 'flex';
+                } else {
+                    alert('Submission failed: ' + result.message);
+                }
+            } catch (error) {
+                console.error('Submission error:', error);
+                alert('Failed to submit the form. Please try again.');
+            }
         });
-
-        const result = await response.json();
-        if (result.success) {
-            document.getElementById('contactForm').style.display = 'none';
-            document.getElementById('thankYouMessage').style.display = 'flex';
-        } else {
-            alert('Submission failed: ' + result.message);
-        }
-    } catch (error) {
-        console.error('Submission error:', error);
-        alert('Failed to submit the form. Please try again.');
     }
+
 });
-
-
-
-
-
